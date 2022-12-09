ecc67c9ca6648e18c094ee48adc5f2de966c1b92
user1 <backenduz@gmail.com>
2022-12-09 22:31:33 +0500

    rm max

--- list_mod11.py
+++ list_mod11.py
@@ -7,4 +7,16 @@ def remove_max(lst):
     Returns:
         list: list without maximum numbers.
     '''
-    return
+    ls = []
+    mx = 0
+
+    for i in range(len(lst)):
+        if lst[i] > mx:
+            mx = lst[i]
+    i = 0
+    while i < len(lst):
+        if lst[i] != mx:
+            ls.append(lst[i])
+        i += 1
+    return ls
+print(remove_max([1, 2, 3, 4, 5]))
