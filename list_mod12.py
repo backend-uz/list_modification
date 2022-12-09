32186b28b7de8457fc4a07139cecadf2451fdd17
user1 <backenduz@gmail.com>
2022-12-09 22:39:10 +0500

    rm min

--- list_mod12.py
+++ list_mod12.py
@@ -7,4 +7,16 @@ def remove_min(lst):
     Returns:
         list: list wihout minimum numbers.
     '''
-    return
+    mn = 0
+    ls = []
+
+    for i in range(len(lst)):
+        if lst[i] < lst[1]:
+            mn = lst[i]
+    i = 0
+    while i < len(lst):
+        if lst[i] != mn:
+            ls.append(lst[i])
+        i += 1
+    return ls
+print(remove_min([2, 3, 4, 5]))
